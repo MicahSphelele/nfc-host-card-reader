45fd9f06e53e679f6062d4d03aef63938c8a061b
sphelele95 <sphemicah@gmail.com>
2021-04-16 17:15:16 +0200

    💉 Made changes to the EmvReader
    - Implemented FCI command and response
    - Got application label using FCI response
    - Implemented PDOL command and response
    - Got GPO command using PDOL response
    - Got Application (Primary Account  Number) for supported Visa cards
    - Got cardholder name for supported Visa cards
    - Got expiration date for supported Visa cards
    - Got AFL data using GPO
    - 👍Started working on the cards records data

--- emv-core/src/main/java/com/smn/emvcore/task/EmvReader.java
+++ emv-core/src/main/java/com/smn/emvcore/task/EmvReader.java
@@ -5,14 +5,19 @@ import com.smn.emvcore.enums.EmvCardType;
 import com.smn.emvcore.interfaces.ResultsListener;
 import com.smn.emvcore.interfaces.Transceiver;
 import com.smn.emvcore.logger.EmvLogger;
+import com.smn.emvcore.model.AflObject;
+import com.smn.emvcore.utils.AflUtil;
 import com.smn.emvcore.utils.AidUtil;
+import com.smn.emvcore.utils.DolUtil;
 import com.smn.emvcore.utils.EmvUtil;
+import com.smn.emvcore.utils.GpoUtil;
 import com.smn.emvcore.utils.HexUtil;
 import com.smn.emvcore.utils.PseUtil;
 import com.smn.emvcore.utils.TlvUtil;
 
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
+import java.util.List;
 
 public class EmvReader implements Runnable {
 
@@ -31,6 +36,7 @@ public class EmvReader implements Runnable {
 
         boolean isPayPass = false;
         boolean isPayWave = false;
+        boolean aidNotSupported = false;
 
         // Thread relative
         // ATS (Answer To Select)
@@ -195,7 +201,7 @@ public class EmvReader implements Runnable {
                                 aid = resultRes;
                                 this.emvLogger.info("AID Visa Electron = " + HexUtil.bytesToHexadecimal(aid));
                             } else {
-                                this.emvLogger.error("Cannot identify card = " + HexUtil.bytesToHexadecimal(resultRes));
+                                this.emvLogger.error("Cannot identify AID = " + HexUtil.bytesToHexadecimal(resultRes));
                             }
                         }
                     }
@@ -274,7 +280,8 @@ public class EmvReader implements Runnable {
                                     this.emvLogger.info("AID Visa Electron = " + HexUtil.bytesToHexadecimal(aid));
                                 } else {
                                     aid = resultRes;
-                                    this.emvLogger.error("Cannot identify card = " + HexUtil.bytesToHexadecimal(resultRes));
+                                    aidNotSupported = true;
+                                    this.emvLogger.error("Cannot identify AID = " + HexUtil.bytesToHexadecimal(resultRes));
                                 }
                             }
                         }
@@ -290,8 +297,13 @@ public class EmvReader implements Runnable {
             }
         }
 
+        if (aidNotSupported) {
+            listener.onError("Card AID type not supported");
+            return;
+        }
+
         // FCI (File Control Information)
-        byte[] commFileControlInfor = null;
+        byte[] commFileControlInfor;
         byte[] respFileControlInfor = null;
 
         if (Arrays.equals(aid, EmvCardType.Mastercard.getAidPrefix())) {
@@ -361,11 +373,17 @@ public class EmvReader implements Runnable {
         }
 
         if (commFileControlInfor != null) {
-            emvLogger.info("EMV (C-APDU) - Command: Select FCI (File Control Information); Data: FCI (File Control Information) Hexadecimal: " + HexUtil.bytesToHexadecimal(commFileControlInfor));
+            this.emvLogger.info("EMV (C-APDU) - Command: Select FCI (File Control Information); Data: FCI (File Control Information) Hexadecimal: " + HexUtil.bytesToHexadecimal(commFileControlInfor));
         }
 
         if (respFileControlInfor != null) {
-            emvLogger.info("EMV (R-APDU) - Command: Select FCI (File Control Information); Data:  Hexadecimal: " + HexUtil.bytesToHexadecimal(respFileControlInfor));
+            this.emvLogger.info("EMV (R-APDU) - Command: Select FCI (File Control Information); Data:  Hexadecimal: " + HexUtil.bytesToHexadecimal(respFileControlInfor));
+        }
+
+        if (!EmvUtil.isOk(respFileControlInfor)) {
+            listener.onError("Invalid FCI data");
+            emvLogger.error("Invalid File Control Information");
+            return;
         }
 
         // Application Label (May be ASCII convertible)
@@ -374,9 +392,173 @@ public class EmvReader implements Runnable {
             applicationLabel = new TlvUtil().getTlvValue(respFileControlInfor, EMVTags.APPLICATION_LABEL.getBytes());
 
             if (applicationLabel != null) {
-                emvLogger.info("Application Label  = " + applicationLabel.length);
+                this.emvLogger.info("EMV (TLV) Application Label  [50] = " + HexUtil.bytesToHexadecimal(applicationLabel));
+            }
+        }
+
+        // PDOL (Processing Options Data Object List)
+        byte[] processingDataObjectList = null;
+        byte[] tempProcessingDataObjectList = new TlvUtil().getTlvValue(respFileControlInfor, EMVTags.PDOL.getBytes());
+
+        if (tempProcessingDataObjectList != null &&
+                DolUtil.isValidDol(tempProcessingDataObjectList, EMVTags.PDOL.getBytes())) {
+
+            processingDataObjectList = tempProcessingDataObjectList;
+            emvLogger.info("EMV (TLV) - Data:  [9F38] Hexadecimal: " + HexUtil.bytesToHexadecimal(processingDataObjectList));
+        }
+
+        // PDOL Constructed
+        byte[] processingDOListConstructed = new GpoUtil().fillPdol(processingDataObjectList);
+
+        if (processingDOListConstructed != null) {
+            this.emvLogger.info("EMV (TLV) - Data:  Constructed : " + Arrays.toString(processingDOListConstructed));
+            this.emvLogger.info("EMV (TLV) - Data:  Constructed Hexadecimal : " + HexUtil.bytesToHexadecimal(processingDOListConstructed));
+        }
+
+        // GPO (Get Processing Options)
+        byte[] commGetProcessingOptions = new GpoUtil().cGpo(processingDOListConstructed);
+        byte[] respGetProcessingOptions = null; // C-APDU & R-APDU
+
+        if (commGetProcessingOptions != null) {
+            this.emvLogger.info("EMV (C-APDU) - Command: Get Data; GPO (Get Processing Options) Data: " +
+                    Arrays.toString(commGetProcessingOptions));
+            this.emvLogger.info("EMV (C-APDU) - Command: Get Data; GPO (Get Processing Options) Data: Hexadecimal: "
+                    + HexUtil.bytesToHexadecimal(commGetProcessingOptions));
+            try {
+                respGetProcessingOptions = this.transceiver.transceive(commGetProcessingOptions);
+            } catch (Exception e) {
+                this.emvLogger.error(e);
+            }
+        }
+
+        if (respGetProcessingOptions != null) {
+
+            this.emvLogger.info("EMV (R-APDU) - Command: Get Data; GPO (Get Processing Options) Data: Hexidecimal " +
+                    HexUtil.bytesToHexadecimal(respGetProcessingOptions));
+
+            if (EmvUtil.isOk(respGetProcessingOptions)) {
+                this.emvLogger.info("EMV (R-APDU) - Command: Get Data; GPO (Get Processing Options) Data: Succeeded");
+            }
+        }
+
+        //PayWave Only
+        if (isPayWave) {
+            // Application PAN (Primary Account Number)
+            if (applicationPan == null) {
+
+                applicationPan = new TlvUtil().getTlvValue(respGetProcessingOptions, EMVTags.APPLICATION_PAN.getBytes());
+
+                if (applicationPan != null) {
+                    this.emvLogger.info("EMV (TLV) - Data: Application Account Number  [5A]: " +
+                            Arrays.toString(applicationPan));
+                    this.emvLogger.info("EMV (TLV) - Data: Application Account Number  [5A] Hexadecimal: " +
+                            HexUtil.bytesToHexadecimal(applicationPan));
+                }
+            }
+            // Application PAN (Primary Account Number)
+            // Cardholder name
+            if (cardholderName == null) {
+                cardholderName = new TlvUtil().getTlvValue(respGetProcessingOptions, EMVTags.CARDHOLDER_NAME.getBytes());
+                if (cardholderName != null) {
+                    this.emvLogger.info("EMV (TLV) - Data: Card holder name [5F20] : " + Arrays.toString(cardholderName));
+
+                    String cardholderNameHexadecimal = HexUtil.bytesToHexadecimal(cardholderName);
+                    if (cardholderNameHexadecimal != null) {
+                        this.emvLogger.info("EMV (TLV) - Data: Card holder name [5F20] Hexadecimal : " + cardholderNameHexadecimal);
+                    }
+                }
             }
 
+            // Application Expiration Date
+            if (applicationExpirationDate == null) {
+
+                applicationExpirationDate = new TlvUtil().getTlvValue(processingDataObjectList, EMVTags.APPLICATION_EXPIRATION_DATE.getBytes());
+
+                if (applicationExpirationDate != null) {
+                    this.emvLogger.info("EMV (TLV) - Data: Application Expiration Date [5F24]: " + Arrays.toString(applicationExpirationDate));
+
+                    String applicationExpirationDateHexadecimal = HexUtil.bytesToHexadecimal(applicationExpirationDate);
+                    if (applicationExpirationDateHexadecimal != null) {
+                        this.emvLogger.info("EMV (TLV) - Data: Application Expiration Date [5F24] Hexadecimal: " + applicationExpirationDateHexadecimal);
+                    }
+                }
+            }
+            // Application Expiration Date
+        }
+        // - PayWave Only
+
+        // AFL (Application File Locator) [GPO] Data
+        byte[] appFileLocatorData = null;
+
+        // Response message template 1 (without tags and lengths)
+        if (respGetProcessingOptions[0] == EMVTags.GPO_RMT1.getBytes()[0]) {
+            this.emvLogger.info("GPO (Get Processing Options) Response message template 1");
+            byte[] gpoData80 = null;
+        }
+        // - Response message 1 (without tags and lengths)
+
+        // Response message template 2 (with tags and lengths)
+        if (respGetProcessingOptions[0] == EMVTags.GPO_RMT2.getBytes()[0]) {
+            this.emvLogger.info("GPO (Get Processing Options) Response message template 2");
+            byte[] gpoData77 = new TlvUtil().getTlvValue(respGetProcessingOptions, EMVTags.GPO_RMT2.getBytes());
+            if (gpoData77 != null) {
+                // AFL (Application File Locator)
+                byte[] afl; // TLV (Type-length-value) tag specified for AFL (Application File Locator) and result variable
+
+                afl = new TlvUtil().getTlvValue(respGetProcessingOptions, EMVTags.AFL.getBytes());
+
+                if (afl != null) {
+                    appFileLocatorData = afl;
+                }
+                // - AFL (Application File Locator)
+            }
+        }
+        // - Response message template 2 (with tags and lengths)
+
+        // - AFL (Application File Locator) [GPO] Data
+        if (appFileLocatorData != null) {
+            this.emvLogger.info("EMV (TLV) - Data: AFL (Application File Locator) [94]: " + Arrays.toString(appFileLocatorData));
+
+            String alfDataHexadecimal = HexUtil.bytesToHexadecimal(appFileLocatorData);
+            if (alfDataHexadecimal != null) {
+                this.emvLogger.info("EMV (TLV) - Data: AFL (Application File Locator) [94] Hexadecimal: " + alfDataHexadecimal);
+            }
+        }
+        // - AFL (Application File Locator) [GPO] Data
+
+        // CDOL1 (Card Risk Management Data Object List 1) & CDOL2 (Card Risk Management Data Object List 2)
+        byte[] cardRiskDataObjectList_1 = null;
+        byte[] cardRiskDataObjectList_2 = null;
+
+        List<AflObject> appFileLocatorObjectList = new AflUtil().getAflDataRecords(appFileLocatorData);
+
+        // AFL (Application File Locator) Record(s)
+        if (!appFileLocatorObjectList.isEmpty() && appFileLocatorObjectList.size() > 0) {
+
+            for (AflObject aflObject : appFileLocatorObjectList) {
+                byte[] commReadRecord = aflObject.getReadCommand();
+                byte[] respReadRecord = null;
+
+                if (commReadRecord != null) {
+
+                    this.emvLogger.info("EMV (C-APDU) - Command: \"Read Record\"; Data: \"Read Record\" Hexadecimal: " +
+                            HexUtil.bytesToHexadecimal(commReadRecord));
+
+                    try {
+                        respReadRecord = this.transceiver.transceive(commReadRecord);
+                    } catch (Exception e) {
+                        this.emvLogger.error(e);
+                    }
+                }
+
+                if (respReadRecord != null) {
+
+                    boolean succeedLe = false;
+
+                    this.emvLogger.info("EMV (R-APDU) - Command: \"Read Record\"; Data: \"Read Record\" Hexadecimal: " +
+                            HexUtil.bytesToHexadecimal(respReadRecord));
+                }
+            }
         }
 
         if (aid != null) {
