99bd212cae6646a733a1eef255c59d9ffea0ce40
sphelele95 <sphemicah@gmail.com>
2021-04-18 18:06:53 +0200

    Removed unused variables in AidUtil 🧹

--- emv-core/src/main/java/com/smn/emvcore/utils/AidUtil.java
+++ emv-core/src/main/java/com/smn/emvcore/utils/AidUtil.java
@@ -8,70 +8,7 @@ import org.jetbrains.annotations.Nullable;
 import java.io.ByteArrayOutputStream;
 
 public class AidUtil {
-    /*
-        Mastercard (PayPass)
-        RID: A000000004
-        PIX: 1010
-        AID (Application Identifier): A0000000041010
-     */
-    public static byte[] A0000000041010 = {
-            (byte) 0xA0,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x04,
-            (byte) 0x10,
-            (byte) 0x10
-    };
-
-    /*
-        Maestro (PayPass)
-        RID: A000000004
-        PIX: 3060
-        AID (Application Identifier): A0000000043060
-     */
-    public static byte[] A0000000043060 = {
-            (byte) 0xA0,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x04,
-            (byte) 0x30,
-            (byte) 0x60
-    };
-
-    /*
-        Visa (PayWave)
-        RID: A000000003
-        PIX: 1010
-        AID (Application Identifier): A0000000031010
-     */
-    public static byte[] A0000000031010 = {
-            (byte) 0xA0,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x03,
-            (byte) 0x10,
-            (byte) 0x10
-    };
-
-    /*
-        Visa Electron (PayWave)
-        RID: A000000003
-        PIX: 2010
-        AID (Application Identifier): A0000000032010
-     */
-    public static byte[] A0000000032010 = {
-            (byte) 0xA0,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x00,
-            (byte) 0x03,
-            (byte) 0x20,
-            (byte) 0x10
-    };
-
+    
     @Nullable
     public static byte[] selectAid(@NotNull byte[] aid) {
         // Returning result
