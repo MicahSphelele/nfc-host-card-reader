d1c08df33138d5de512e620aed6568a378149b1b
sphelele95 <sphemicah@gmail.com>
2021-04-19 16:10:59 +0200

    Code formatting in  NfcState 🛰

--- cardreaderlibrary/src/main/java/com/smn/cardreaderlib/enums/NfcState.java
+++ cardreaderlibrary/src/main/java/com/smn/cardreaderlib/enums/NfcState.java
@@ -1,5 +1,5 @@
 package com.smn.cardreaderlib.enums;
 
-public enum  NfcState {
-    NFC_ENABLED,NFC_NOT_ENABLED, NFC_NOT_SUPPORTED
+public enum NfcState {
+    NFC_ENABLED, NFC_NOT_ENABLED, NFC_NOT_SUPPORTED
 }
