6b228bc2744814650aa69f7e9396a3ce281eedff
sphelele95 <sphemicah@gmail.com>
2021-04-20 10:39:57 +0200

    Updated CardTypeEnum ✔

---
+++ emv-core/src/main/java/com/smn/emvcore/handler/MiscHandler.java
@@ -0,0 +1,12 @@
+package com.smn.emvcore.handler;
+
+import com.smn.emvcore.logger.EmvLogger;
+
+public class MiscHandler {
+
+    private final EmvLogger logger;
+
+    public MiscHandler(EmvLogger logger) {
+        this.logger = logger;
+    }
+}
