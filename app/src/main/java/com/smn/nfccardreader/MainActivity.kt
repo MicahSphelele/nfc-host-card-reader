246534ed8468e472dc3cd0be274bcf7bd5eac80f
sphelele95 <sphemicah@gmail.com>
2021-04-21 10:02:22 +0200

    Implemented view binding in Main App
    - Added layout tag in activity_main.xml
    - Added a string in strings.xml
    - Made view binding changes in MainActivity

--- app/src/main/java/com/smn/nfccardreader/MainActivity.kt
+++ app/src/main/java/com/smn/nfccardreader/MainActivity.kt
@@ -5,40 +5,47 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.smn.cardreaderlib.NFCReaderSDK
 import com.smn.cardreaderlib.enums.NfcState
 import com.smn.cardreaderlib.interfaces.EmvResultsListener
 import com.smn.cardreaderlib.models.EmvResults
+import com.smn.nfccardreader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        NFCReaderSDK.getInstance().nfcDevice.startCardReader( this,object :
-            EmvResultsListener {
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-            override fun onEmvResults(emvResults: EmvResults) {
-                runOnUiThread {
-                    findViewById<TextView>(R.id.text).text = String.format("AID = %s \nCardNumber = %s",
-                        emvResults.aid,emvResults.cardNumber)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NFCReaderSDK.getInstance().nfcDevice.startCardReader( this,object :
+                EmvResultsListener {
+
+                override fun onEmvResults(emvResults: EmvResults) {
+                    runOnUiThread {
+                        binding.text.text = String.format("AID = %s \nCardNumber = %s",
+                            emvResults.aid,emvResults.cardNumber)
+                    }
                 }
-            }
 
-            override fun onNfcState(nfcState: NfcState) {
-                runOnUiThread {
-                    findViewById<TextView>(R.id.text).text = String.format("%s",nfcState.name)
+                override fun onNfcState(nfcState: NfcState) {
+                    runOnUiThread {
+                        binding.text.text = String.format("%s",nfcState.name)
+                    }
                 }
-            }
 
-            override fun onError(message: String) {
-                runOnUiThread {
-                    findViewById<TextView>(R.id.text).text = message
+                override fun onError(message: String) {
+                    runOnUiThread {
+                        binding.text.text = message
+                    }
                 }
-            }
 
-        })
+            })
+            return
+        }
+        binding.text.text = this.getString(R.string.txt_nfc_error_message)
     }
 }
